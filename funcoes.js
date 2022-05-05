af6d0cf2ca376c5225a69ba63ee698444761c92a
vinik1ng <102757784+vinik1ng@users.noreply.github.com>
2022-05-05 15:43:06 -0300

    Add files via upload

---
+++ funcoes.js
@@ -0,0 +1,37 @@
+//soma
+
+
+
+//1
+function nome (a){
+    console.log(a)
+    return
+}
+
+nome('Vinicius')
+
+//2
+function soma (a,b){
+    let res = a+b;
+    console.log(res)
+    return
+}
+
+soma(5,3)
+
+//3
+function multiplicação (c,d){
+    console.log(c*d)
+    return
+}
+
+multiplicação(8,9)
+
+//4
+function média(w,x,y,z){
+    let med = w+x+y+z;
+    console.log((med)/4)
+    return
+}
+
+média(1,2,3,4)
